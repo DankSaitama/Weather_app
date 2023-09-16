66def6493b56910c1c582ff8b9bc0c881abb8b36
Hirtik <hirtikpatelhp321@gmail.com>
2023-09-17 01:11:54 +0530

    first commit

---
+++ Softices/weatherapp/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.conf import settings
+import requests
+
+
+# Create your views here.
+
+def index(request):
+    return render(request, "index.html")
+
+def weather(request):
+    if request.method == 'POST':
+        # Handle the form submission
+        location = request.POST.get('location', '')  # Get the user's input
+        api_key = settings.API_KEY
+
+        if location:
+            # Construct the API request URL
+            url = f'http://api.openweathermap.org/data/2.5/weather?q={location}&units=metric&appid={api_key}'
+
+            response = requests.get(url)
+            data = response.json()
+
+            if data.get('cod') == 200:
+                # Successfully retrieved weather data
+                city_name = data.get('name')
+                weather = data['weather'][0]['main']
+                temperature = round(data['main']['temp'])
+                description = data['weather'][0]['description']
+
+                context = {
+                    'city': city_name,
+                    'weather_condition': weather,
+                    'temperature': temperature,
+                    'description': description,
+                }
+
+                return render(request, 'weather.html', context)
+            else:
+                # Handle the case where the city is not found
+                return render(request, 'weather.html', {'error_message': 'No City Found'})
+        else:
+            # Handle the case where the user didn't provide input
+            return render(request, 'weather.html', {'error_message': 'Please enter a city name or pincode.'})
+    else:
+        # Render the initial weather input form
+        return render(request, 'weather_form.html')
+
