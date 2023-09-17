795d9b26871e7808b00474352ea78e8a23852e1e
DankSaitama <132942381+DankSaitama@users.noreply.github.com>
2023-09-17 12:29:11 +0530

    Add files via upload

---
+++ static/JS/script.js
@@ -0,0 +1,17 @@
+console.log('working')
+document.addEventListener("DOMContentLoaded", function () {
+    const bodyElement = document.body;
+    const weatherConditionInput = document.getElementById("weather-condition");
+
+    // Function to update the body class based on weather condition
+    function updateBackground() {
+        const weatherCondition = weatherConditionInput.value;
+        bodyElement.className = `weather-${weatherCondition.toLowerCase()}`;
+    }
+
+    // Initial background update
+    updateBackground();
+
+    // You can also call updateBackground() whenever the weather condition changes.
+    // Example: In your AJAX success callback when fetching weather data.
+});
